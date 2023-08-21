c407429ddfe796366b673ab3101e76fe4471c790
t-hirata74 <t.hirata.0463@gmail.com>
2023-08-21 23:03:41 +0900

    v-modelの定義を追加

--- app/node/tutorial/src/App.vue
+++ app/node/tutorial/src/App.vue
@@ -22,34 +22,34 @@
 <div>
   <p class="bg-blue-200 text-2xl p-4">Hello World!</p>
   <div class="container mx-auto mt-4">
-    <select class="select select-primary mb-4">
+    <select class="select select-primary mb-4" v-model="features.Sex">
     <option disabled selected>性別は？</option>
     <option>男性</option>
     <option>女性</option>
     </select>
     <br>
-    <select class="select select-primary mb-4">
+    <select class="select select-primary mb-4" v-model="features.Pclass">
       <option disabled selected>階級は？</option>
       <option>上層クラス（お金持ち）</option>
       <option>中級クラス（一般階級）</option>
       <option>下層クラス（労働階級）</option>
     </select>
     <br>
-    <select class="select select-primary mb-4">
+    <select class="select select-primary mb-4" v-model="features.Age">
       <option disabled selected>年齢は？</option>
       <option v-for="i in [...Array(121).keys()]" :key="i.id">
         {{ i }}
         </option>
     </select> 歳
     <br>
-    <select class="select select-primary mb-4">
+    <select class="select select-primary mb-4" v-model="features.Parch">
       <option disabled selected>親・子の同伴者数は？</option>
       <option v-for="i in [...Array(11).keys()]" :key="i.id">
         {{ i }} 
       </option>
     </select> 人
     <br>
-    <select class="select select-primary mb-4">
+    <select class="select select-primary mb-4" v-model="features.SibSp">
       <option disabled selected>兄弟姉妹の同伴者数は？</option>
       <option v-for="i in [...Array(11).keys()]" :key="i.id">
         {{ i }} 
